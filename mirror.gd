4403fd2e516ad1bb6ede1deaf34b875a159ca7b3
coskar <oskar.cokl@gmail.com>
2023-07-12 20:53:48 +0200

    feat: Got Mirror effect to work well.

--- mirror.gd
+++ mirror.gd
@@ -1,16 +1,39 @@
 extends Node3D
 
-@onready var player = get_tree().get_root().get_node("Main/Player")
+@onready var player: CharacterBody3D = get_tree().get_root().get_node("Main/Player")
+@onready var player_camera: Camera3D = player.get_node("Camera3D")
 @onready var sub_viewport = $MirrorMesh/SubViewport
 @onready var mirror_camera = $MirrorMesh/SubViewport/Camera3D
 @onready var mirror_mesh: MeshInstance3D = $MirrorMesh
 
+
+var plane: Plane
+
 func _ready():
     sub_viewport.size = Vector2(2000, 1000)
+    define_plane()
 
 
 func _process(_delta):
-    pass
+    var projection_on_plane = plane.project(player.global_position)
+    var reflected_position = 2 * projection_on_plane - player.global_position
+    mirror_camera.global_position = reflected_position
+    mirror_camera.global_position.y = player_camera.global_position.y
+    print(mirror_camera.global_rotation_degrees)
+    var player_global_rotation = player_camera.global_rotation
+    mirror_camera.global_rotation = Vector3(player_global_rotation.x, -player_global_rotation.y, player_global_rotation.z)
+
+
+func define_plane():
+    var mdt: MeshDataTool = MeshDataTool.new()
+    var arr_mesh: ArrayMesh = ArrayMesh.new()
+    arr_mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, mirror_mesh.mesh.get_mesh_arrays())
+    mdt.create_from_surface(arr_mesh, 0)
+    var plane_normal: Vector3 = mdt.get_face_normal(0)
+    var plane_normal_rotated: Vector3 = (quaternion * plane_normal)
+    var distance_from_origin: float = global_position.distance_to(Vector3(0, 0, 0))
+    plane = Plane(plane_normal_rotated, -distance_from_origin)
+
 
 func show_mirror_normals():
     var vertices = mirror_mesh.mesh.get_faces()
@@ -46,8 +69,3 @@ func show_mirror_normals():
     debug_mesh.mesh = immediate_mesh
     mirror_mesh.add_child(debug_mesh)
 
-
-func update_cam(player_cam_transform):
-    print(player.position.distance_to(get_parent().get_parent().position))
-    global_transform = player_cam_transform
-    global_position.x += 10
